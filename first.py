65ca07ec0aba7b0876040ec65409f1fc8078cb2a
rajesh-sahoo4 <sahoorajesh7684@gmail.com>
2026-02-08 23:28:55 +0530

    add file

---
+++ first.py
@@ -0,0 +1,144 @@
+''' String'''
+# print("hello world")
+# print("rajesh_sahoo")
+# name ="Rajesh"
+# age = 25
+# price =25.10
+# print (name,age,price)
+# print(type(name))
+
+'''String Method'''
+# var1="python tester"
+# print(var1.capitalize())
+# first word captilize 
+
+# print(var1.title())
+# First character of each word
+
+# print(var1.islower())
+# is lower case word yes/no
+
+# print(var1.isupper())
+# is upper case word yes/no
+
+# print(var1.lower())
+# convert to upper case to lower 
+
+# print(var1.upper())
+# convert to lower case to upper
+
+# print(len(var1))
+# lenth of word
+
+# print(var1.count("t"))
+# kitana bar t hai this variable
+
+# print(var1.find(' '))
+# print(var1.find("t"))
+# serch index number of character
+
+# print(var1.find('t',3)) 
+#
+
+# print(var1.split())
+# # multiple word divided to each string with list
+
+# print(var1.swapcase())
+# convert to all word uppercase to lowercase (interchange)
+
+# print(var1.replace('tester','language'))
+# replacing data
+
+# s='ram is a good person'
+# s1=s.split()
+# print(s1)
+ 
+"""
+indentation Error - Spacebar add hote hai error ata hai. 
+"""
+# var2="python automation"
+# print(var2.title())
+
+
+# print(var2.isupper())
+# que="python is very easy"
+# print(que.strip())
+# que1='python 123'
+# print(que1.isalpha())
+# # que2='treenetra@gmail.com'
+
+# que3='@@@python automation'
+# print(que3.lstrip('@'))
+
+# s='pyhton coding'
+# print(s[3:5]) 
+# print(s[3:12:2])
+# s=variable , 3=start index , 5=end index and 2=step 
+# this is called forward index (by defult)
+# 
+# print(s[-2:-10:-1])
+# this is called backword index 
+# -2 start index ,-10 end index &
+# print(s[0:10])
+
+# s1='Python is very simple and easy to learn' #simple and easy
+# s2=s1.split()
+# s3=s2[3:6]
+# s4=' '.join(s3)
+
+# q='programming' #margor pormig
+# print(q[-4:-10:-1])
+# print(q[:10:2])
+
+ 
+# s6='programming language like java is very difficult'
+# s7=s6[::-1]
+# s8=''.joi(s7)
+# print(s8)
+
+# 0 1 2 3 4 5
+
+'''List'''
+
+# list is  a collection of elements .these are may be homogenious or hetrogenious .
+#  s=[10,'Rajesh',20]
+# homogenious and hetrogenious
+# 3 type we create list
+# list(),[],range()
+#example of 
+# l='list'
+# print(list(l))
+# list concation 
+# exm-
+# l1=[10,20],l2=[20,30,40] l3=l1+l2
+
+'''List methods'''
+
+# 1)len()-count length
+
+# 2)count()-perticular one element find in list
+
+# index()-check index position 
+
+# append()-item add end of exiting list.
+# only one element added in exiting list data.
+# exm_1-l1=[10,20,30] 
+# print(l1.append(50))--[10,20,30,50]
+# exm_2-l1.append([50,60,70])
+#  print(l1)--[10,20,30,[50,60,70]] 
+
+# Extend()-two list add (end main add hoga but one by one element )
+# exm--L1=[10,20,30]
+# print(L1.extend([20,30,50,60]))---[10,20,30,20,30,50,60]
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sec.py
@@ -0,0 +1,87 @@
+'''Pratice Question of Slice,Split and join'''
+# txt='python'
+# print(txt[0:4])
+# print(txt[2:])
+# print(txt[:3])
+# print(txt[::2])
+# print(txt[::-1])
+# print(txt[-2:-5:-1])
+
+# easy
+ 
+msg='i love python'
+msg1=msg.split()
+msg2=' '.join(msg1)
+msg3=msg[::-1]
+print(msg3)
+# Q1
+exm='programming'
+exm1=exm[0:7]
+print(exm1)
+# Q2
+exm2='Python'
+print(exm2[::-1])
+# Q3
+exm3='apple mango banana'
+print(exm3.split())
+
+# medium
+# Q4
+num='10-20-30-40'
+print(num.split("-"))
+# Q5
+wrd=['Hello','World']
+print(' '.join(wrd))
+# Q6
+wrd2='softwareTesting'
+print(wrd2[8:])
+
+#Hard
+# Q7
+wrd3='2026/02/04'
+wrd4=wrd3.split('/')
+wrd5='-'.join(wrd4)
+print(wrd5)
+# Q8
+sen='I_am_learning_Python'
+sen1=sen.split('_')
+sen2=' '.join(sen1)
+print(sen2)
+# Q9
+tch='Technology'
+print(tch[0:4:])
+# Q10
+dv='Developer'
+print(dv[::-1])
+# Q11
+anm='dog cat cow goat'
+print(anm.split())
+# Q12
+lng=['Python','is','easy']
+print(' '.join(lng))
+# Q13
+clr="red,green,blue,yellow"
+print(clr.split(','))
+# Q14
+word='ManualTesting'
+print(word[6::])
+# Q15
+nm=['10','20','30']
+print(':'.join(nm))
+# Q16
+usr='user-name-email'
+print(usr.split('-'))
+# Q17
+dte='2026|03|15'
+dte1=(dte.split("|"))
+dte2=('-'.join(dte1))
+print(dte2) 
+
+s1='Python is very simple and easy to learn' #simple and easy
+s2=s1.split()
+s3=s2[3:6:]
+s4=(' '.join(s3))
+print(s4)
+
+'''Pratice question of List'''
+
